542b18df3dee09acfb2d4f2123a44e46e79791c3
oasispoudel <oasispoudel@gmail.com>
2026-04-18 12:07:11 +0530

    avg cost calculation

--- src/com/simulator/model/portfolio.java
+++ src/com/simulator/model/portfolio.java
@@ -11,6 +11,7 @@ import static com.simulator.model.Side.BUY;
 public class portfolio {
     private double cashBalance;
     private Map<String,Integer> holdings = new HashMap<>();
+    private Map<String,Double> avgCost = new HashMap<>();
     private List<transaction> history = new ArrayList<>();
 
     public portfolio(double startingCash) {
@@ -22,6 +23,10 @@ public class portfolio {
         if(cost <= cashBalance) {
             cashBalance -= cost;
             int currentQty = holdings.getOrDefault(ticker, 0);
+            double oldAvg = avgCost.getOrDefault(ticker,0.0);
+            double newAvg = (currentQty * oldAvg + quantity * price)/(currentQty+quantity);
+            avgCost.put(ticker, newAvg);
+
             holdings.put(ticker, currentQty + quantity);
             history.add(new transaction(ticker,Side.BUY,quantity,price, LocalDateTime.now()));
 
@@ -35,7 +40,11 @@ public class portfolio {
         if(holdings.getOrDefault(ticker,0) >= quantity) {
             int currentQty = holdings.getOrDefault(ticker, 0);
             cashBalance += quantity * price;
+            int remainingQty = currentQty - quantity;
             holdings.put(ticker, currentQty - quantity);
+            if(remainingQty == 0){
+                avgCost.remove(ticker);
+            }
             history.add(new transaction(ticker,Side.SELL,quantity,price, LocalDateTime.now()));
 
         }
@@ -53,6 +62,10 @@ public class portfolio {
     public Map<String, Integer> getHoldings() {
         return new HashMap<>(holdings);
     }
+    public Map<String,Double> getAvgCost(){
+        return new HashMap<>(avgCost);
+        }
+
     public double getTotalValue(Map<String, Double> livePrices) {
         double sum = 0;
         for(String ticker: holdings.keySet()){
