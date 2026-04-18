3594fb8d168475518006de251395b2317a3d9369
agp <aagamanpokharel45@gmail.com>
2026-04-18 11:04:48 +0530

    fixed editable transaction history issue

--- src/com/simulator/ui/LedgerPanel.java
+++ src/com/simulator/ui/LedgerPanel.java
@@ -21,7 +21,12 @@ public class LedgerPanel extends JPanel implements PriceObserver {
         setLayout(new BorderLayout());
         
         String[] cols = {"Ticker", "Side", "Qty", "Price", "Time"};
-        tableModel = new DefaultTableModel(cols, 0);
+        tableModel = new DefaultTableModel(cols, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
         table = new JTable(tableModel);
         
         add(new JLabel("Transaction History", SwingConstants.CENTER), BorderLayout.NORTH);
